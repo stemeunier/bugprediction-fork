3921361734a44668c099d888e0fa80be95b1a639
Yann LE COZ <yann.lecoz@sii.fr>
2022-09-14 14:34:11 +0200

    Add CK value

--- ckconnector.py
+++ ckconnector.py
@@ -43,17 +43,46 @@ class CkConnector:
 
     def compute_metrics(self, version):
         csv_class = pd.read_csv("class.csv")
+        csv_method = pd.read_csv("method.csv")
 
         wmc = self.compute_mean('wmc', csv_class)
         dit = self.compute_mean('dit', csv_class)
         noc = self.compute_mean('noc', csv_class)
         cbo = self.compute_mean('cbo', csv_class)
         lcom = self.compute_mean('lcom', csv_class)
+        lcc = self.compute_mean('lcc', csv_class)
+        loc = self.compute_mean('loc', csv_class)
         fanin = self.compute_mean('fanin', csv_class)
         fanout = self.compute_mean('fanout', csv_class)
         nom = self.compute_mean('totalMethodsQty', csv_class)
         nopm = self.compute_mean('publicMethodsQty', csv_class)
         noprm = self.compute_mean('privateMethodsQty', csv_class)
+        modifiers = self.compute_mean('modifiers', csv_class)
+        nosi = self.compute_mean('nosi', csv_class)
+        rfc = self.compute_mean('rfc', csv_class)
+        tcc = self.compute_mean('tcc', csv_class)
+        cboModified = self.compute_mean('cboModified', csv_class)
+        lcomModified = self.compute_mean('lcom*', csv_class)
+        returnQty = self.compute_mean('returnQty', csv_class)
+        loopQty = self.compute_mean('loopQty', csv_class)
+        tryCatchQty = self.compute_mean('tryCatchQty', csv_class)
+        parenthesizedExpsQty = self.compute_mean('parenthesizedExpsQty', csv_class)
+        numbersQty = self.compute_mean('numbersQty', csv_class)
+        mathOperationsQty = self.compute_mean('mathOperationsQty', csv_class)
+        maxNestedBlocksQty = self.compute_mean('maxNestedBlocksQty', csv_class)
+        anoInnerLambdaQty = self.compute_mean('anonymousClassesQty', csv_class) + self.compute_mean('innerClassesQty', csv_class) + self.compute_mean('lambdasQty', csv_class)
+        uniqueWordsQty = self.compute_mean('uniqueWordsQty', csv_class)
+        logStatementsQty = self.compute_mean('logStatementsQty', csv_class)
+        variablesQty = self.compute_mean('variablesQty', csv_class)
+        comparisonsQty = self.compute_mean('comparisonsQty', csv_class)
+        totalMethodsQty = self.compute_mean('totalMethodsQty', csv_class)
+        visibleMethodsQty = self.compute_mean('visibleMethodsQty', csv_class)
+        totalFieldsQty = self.compute_mean('totalFieldsQty', csv_class)
+        stringLiteralsQty = self.compute_mean('stringLiteralsQty', csv_class)
+
+        hasJavadoc = self.compute_mean("hasJavaDoc", csv_method)
+        methodsInvokedQty = self.compute_mean("methodsInvokedQty", csv_method)
+
 
         # metrics = ['wmc', 'dit', 'noc', 'cbo', 'lcom', 'fanin', 'fanout', 'nom', 'nopm', 'noprm']
         # data = [wmc, dit, noc, cbo, lcom, fanin, fanout, nom, nopm, noprm]
@@ -69,14 +98,40 @@ class CkConnector:
             ck_dit=dit,
             ck_noc=noc,
             ck_cbo=cbo,
+            ck_cbo_modified=cboModified,
             ck_lcom=lcom,
+            ck_lcom_modified=lcomModified,
             ck_fan_in=fanin,
             ck_fan_out=fanout,
+            ck_has_javadoc=hasJavadoc,
+            ck_lcc=lcc,
+            ck_loc=loc,
+            ck_modifiers=modifiers,
+            ck_method_invok=methodsInvokedQty,
             ck_nom=nom,
             ck_nopm=nopm,
             ck_noprm=noprm,
+            ck_nosi=nosi,
+            ck_num_fields=totalFieldsQty,
+            ck_numb_log_stmts=logStatementsQty,
+            ck_num_methods=totalMethodsQty,
+            ck_num_visible_methods=visibleMethodsQty,
+            ck_qty_ano_inner_cls_and_lambda=anoInnerLambdaQty,
+            ck_qty_comparisons=comparisonsQty,
+            ck_qty_loops=loopQty,
+            ck_qty_math_operations=mathOperationsQty,
+            ck_qty_math_variables=variablesQty,
+            ck_qty_nested_blocks=maxNestedBlocksQty,
+            ck_qty_numbers=numbersQty,
+            ck_qty_parenth_exps=parenthesizedExpsQty,
+            ck_qty_returns=returnQty,
+            ck_qty_str_literals=stringLiteralsQty,
+            ck_qty_try_catch=tryCatchQty,  
+            ck_qty_unique_words=uniqueWordsQty,  
+            ck_rfc=rfc,
+            ck_tcc=tcc
         )
 
         self.session.add(metric)
-
+        self.session.commit()
         logging.info("CK metrics added to database fo version " + version.tag)
--- main.py
+++ main.py
@@ -14,10 +14,13 @@ from models.database import setup_database
 from dotenv import load_dotenv
 from githubconnector import GitHubConnector
 from codemaatconnector import CodeMaatConnector
+
+
 # from lizardconnector import LizardConnector
 
 def check_output(command):
-    return subprocess.check_output(command, shell = True).decode("utf-8")
+    return subprocess.check_output(command, shell=True).decode("utf-8")
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -25,20 +28,22 @@ if __name__ == '__main__':
     logging.info('python: ' + platform.python_version())
     logging.info('system: ' + platform.system())
     logging.info('machine: ' + platform.machine())
-    #logging.info('java: ' + check_output("java -version"))
-    
+    # logging.info('java: ' + check_output("java -version"))
+
     engine = db.create_engine(os.environ["OTTM_TARGET_DATABASE"])
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
     setup_database(engine)
-    
-    project = Project(name=os.environ["OTTM_SOURCE_PROJECT"],
-                     repo=os.environ["OTTM_SOURCE_REPO"],
-                     language=os.environ["OTTM_LANGUAGE"])
-    session.add(project)
-    session.commit()
-    
+
+    project = session.query(Project).filter(Project.name == os.environ["OTTM_SOURCE_PROJECT"]).first()
+    if not project:
+        project = Project(name=os.environ["OTTM_SOURCE_PROJECT"],
+                        repo=os.environ["OTTM_SOURCE_REPO"],
+                        language=os.environ["OTTM_LANGUAGE"])
+        session.add(project)
+        session.commit()
+
     # Populate the database
     git = GitHubConnector(
         os.environ["OTTM_GITHUB_TOKEN"],
@@ -51,14 +56,14 @@ if __name__ == '__main__':
     git.setup_aliases(os.environ["OTTM_AUTHOR_ALIAS"])
 
     # Checkout, execute the tool and inject CSV result into the database
-    #with tempfile.TemporaryDirectory() as tmp_dir:
+    # with tempfile.TemporaryDirectory() as tmp_dir:
     tmp_dir = tempfile.mkdtemp()
     logging.info('created temporary directory: ' + tmp_dir)
-    
+
     # Clone the repository
     process = subprocess.run(["git", "clone", os.environ["OTTM_SOURCE_REPO_URL"]],
-                        stdout=subprocess.PIPE,
-                        cwd=tmp_dir)
+                             stdout=subprocess.PIPE,
+                             cwd=tmp_dir)
     logging.info('Executed command line: ' + ' '.join(process.args))
     repo_dir = os.path.join(tmp_dir, os.environ["OTTM_SOURCE_PROJECT"])
 
@@ -66,8 +71,8 @@ if __name__ == '__main__':
     versions = session.query(Version).all()
     for version in versions:
         process = subprocess.run(["git", "checkout", version.tag],
-                        stdout=subprocess.PIPE,
-                        cwd=repo_dir)
+                                 stdout=subprocess.PIPE,
+                                 cwd=repo_dir)
         logging.info('Executed command line: ' + ' '.join(process.args))
 
         # Get statistics from git log with codemaat
@@ -80,5 +85,5 @@ if __name__ == '__main__':
 
         # Get metrics with CK
         ck = CkConnector(directory=repo_dir, session=session)
+        ck.generate_ck_files()
         ck.compute_metrics(version)
-
