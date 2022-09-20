24b537ce7c777ad280c1e6e1f8a1a430075f1466
g.dubrasquet-duval <guillaume.dubrasquet-duval@sii.fr>
2022-09-20 16:59:10 +0200

    CK Files on temporary directory, reorganize CKConnector

--- connectors/ck.py
+++ connectors/ck.py
@@ -1,8 +1,9 @@
 import logging
 import os
-from os.path import exists
-import tempfile
 import subprocess
+import tempfile
+from asyncio import sleep
+
 import pandas as pd
 
 from configuration import Configuration
@@ -83,62 +84,54 @@ class CkConnector:
             logging.info('Executed command line: ' + ' '.join(process.args))
             logging.info('Command return code ' + str(process.returncode))
 
-            if (exists("class.csv") and exists("method.csv") and exists("field.csv") and exists("variable.csv")):
-                try:
-                    # Read csv files
-                    csv_class = pd.read_csv("class.csv")
-                    csv_method = pd.read_csv("method.csv")
-                    csv_field = pd.read_csv("field.csv")
-                    csv_variable = pd.read_csv("variable.csv")
+            csv_class = pd.read_csv(os.path.join(tmp_dir, "class.csv"))
+            csv_method = pd.read_csv(os.path.join(tmp_dir, "method.csv"))
+            csv_field = pd.read_csv(os.path.join(tmp_dir, "field.csv"))
+            csv_variable = pd.read_csv(os.path.join(tmp_dir, "variable.csv"))
 
-                    # Calculate mean CK values
-                    metric.ck_wmc = self.__compute_mean('wmc', csv_class)
-                    metric.ck_dit = self.__compute_mean('dit', csv_class)
-                    metric.ck_noc = self.__compute_mean('noc', csv_class)
-                    metric.ck_cbo = self.__compute_mean('cbo', csv_class)
-                    metric.ck_lcom = self.__compute_mean('lcom', csv_class)
-                    metric.ck_lcc = self.__compute_mean('lcc', csv_class)
-                    metric.ck_loc = self.__compute_mean('loc', csv_class)
-                    metric.ck_fan_in = self.__compute_mean('fanin', csv_class)
-                    metric.ck_fan_out = self.__compute_mean('fanout', csv_class)
-                    metric.ck_nom = self.__compute_mean('totalMethodsQty', csv_class)
-                    metric.ck_nopm = self.__compute_mean('publicMethodsQty', csv_class)
-                    metric.ck_noprm = self.__compute_mean('privateMethodsQty', csv_class)
-                    metric.ck_modifiers = self.__compute_mean('modifiers', csv_class)
-                    metric.ck_nosi = self.__compute_mean('nosi', csv_class)
-                    metric.ck_rfc = self.__compute_mean('rfc', csv_class)
-                    metric.ck_tcc = self.__compute_mean('tcc', csv_class)
-                    metric.ck_cbo_modified = self.__compute_mean('cboModified', csv_class)
-                    metric.ck_lcom_modified = self.__compute_mean('lcom*', csv_class)
-                    metric.ck_qty_returns = self.__compute_mean('returnQty', csv_class)
-                    metric.ck_qty_loops = self.__compute_mean('loopQty', csv_class)
-                    metric.ck_qty_try_catch = self.__compute_mean('tryCatchQty', csv_class)
-                    metric.ck_qty_parenth_exps = self.__compute_mean('parenthesizedExpsQty', csv_class)
-                    metric.ck_qty_numbers = self.__compute_mean('numbersQty', csv_class)
-                    metric.ck_qty_math_operations = self.__compute_mean('mathOperationsQty', csv_class)
-                    metric.ck_qty_nested_blocks = self.__compute_mean('maxNestedBlocksQty', csv_class)
-                    metric.ck_qty_ano_inner_cls_and_lambda = self.__compute_mean('anonymousClassesQty',
-                                                                                 csv_class) + self.__compute_mean(
-                        'innerClassesQty', csv_class) + self.__compute_mean('lambdasQty', csv_class)
-                    metric.ck_qty_unique_words = self.__compute_mean('uniqueWordsQty', csv_class)
-                    metric.ck_numb_log_stmts = self.__compute_mean('logStatementsQty', csv_class)
-                    metric.ck_qty_math_variables = self.__compute_mean('variablesQty', csv_class)
-                    metric.ck_qty_comparisons = self.__compute_mean('comparisonsQty', csv_class)
-                    metric.ck_num_methods = self.__compute_mean('totalMethodsQty', csv_class)
-                    metric.ck_num_visible_methods = self.__compute_mean('visibleMethodsQty', csv_class)
-                    metric.ck_num_fields = self.__compute_mean('totalFieldsQty', csv_class)
-                    metric.ck_qty_str_literals = self.__compute_mean('stringLiteralsQty', csv_class)
-                    metric.ck_has_javadoc = self.__compute_mean("hasJavaDoc", csv_method)
-                    metric.ck_method_invok = self.__compute_mean("methodsInvokedQty", csv_method)
-                    metric.ck_usage_fields = self.__compute_mean("usage", csv_field)
-                    metric.ck_usage_vars = self.__compute_mean("usage", csv_variable)
+            logging.info('CF files correctly created')
 
-                    logging.info('Metric Calculated')
+            # Calculate mean CK values
+            metric.ck_wmc = self.__compute_mean('wmc', csv_class)
+            metric.ck_dit = self.__compute_mean('dit', csv_class)
+            metric.ck_noc = self.__compute_mean('noc', csv_class)
+            metric.ck_cbo = self.__compute_mean('cbo', csv_class)
+            metric.ck_lcom = self.__compute_mean('lcom', csv_class)
+            metric.ck_lcc = self.__compute_mean('lcc', csv_class)
+            metric.ck_loc = self.__compute_mean('loc', csv_class)
+            metric.ck_fan_in = self.__compute_mean('fanin', csv_class)
+            metric.ck_fan_out = self.__compute_mean('fanout', csv_class)
+            metric.ck_nom = self.__compute_mean('totalMethodsQty', csv_class)
+            metric.ck_nopm = self.__compute_mean('publicMethodsQty', csv_class)
+            metric.ck_noprm = self.__compute_mean('privateMethodsQty', csv_class)
+            metric.ck_modifiers = self.__compute_mean('modifiers', csv_class)
+            metric.ck_nosi = self.__compute_mean('nosi', csv_class)
+            metric.ck_rfc = self.__compute_mean('rfc', csv_class)
+            metric.ck_tcc = self.__compute_mean('tcc', csv_class)
+            metric.ck_cbo_modified = self.__compute_mean('cboModified', csv_class)
+            metric.ck_lcom_modified = self.__compute_mean('lcom*', csv_class)
+            metric.ck_qty_returns = self.__compute_mean('returnQty', csv_class)
+            metric.ck_qty_loops = self.__compute_mean('loopQty', csv_class)
+            metric.ck_qty_try_catch = self.__compute_mean('tryCatchQty', csv_class)
+            metric.ck_qty_parenth_exps = self.__compute_mean('parenthesizedExpsQty', csv_class)
+            metric.ck_qty_numbers = self.__compute_mean('numbersQty', csv_class)
+            metric.ck_qty_math_operations = self.__compute_mean('mathOperationsQty', csv_class)
+            metric.ck_qty_nested_blocks = self.__compute_mean('maxNestedBlocksQty', csv_class)
+            metric.ck_qty_ano_inner_cls_and_lambda = self.__compute_mean('anonymousClassesQty', csv_class) + self.__compute_mean(
+                'innerClassesQty', csv_class) + self.__compute_mean('lambdasQty', csv_class)
+            metric.ck_qty_unique_words = self.__compute_mean('uniqueWordsQty', csv_class)
+            metric.ck_numb_log_stmts = self.__compute_mean('logStatementsQty', csv_class)
+            metric.ck_qty_math_variables = self.__compute_mean('variablesQty', csv_class)
+            metric.ck_qty_comparisons = self.__compute_mean('comparisonsQty', csv_class)
+            metric.ck_num_methods = self.__compute_mean('totalMethodsQty', csv_class)
+            metric.ck_num_visible_methods = self.__compute_mean('visibleMethodsQty', csv_class)
+            metric.ck_num_fields = self.__compute_mean('totalFieldsQty', csv_class)
+            metric.ck_qty_str_literals = self.__compute_mean('stringLiteralsQty', csv_class)
+            metric.ck_has_javadoc = self.__compute_mean("hasJavaDoc", csv_method)
+            metric.ck_method_invok = self.__compute_mean("methodsInvokedQty", csv_method)
+            metric.ck_usage_fields = self.__compute_mean("usage", csv_field)
+            metric.ck_usage_vars = self.__compute_mean("usage", csv_variable)
 
-                    return metric
-                except pd.errors.EmptyDataError:
-                    logging.error("No columns to parse from CK report /version " + self.version.tag)
-                except:
-                    logging.error("An error occured while reading CK report for version " + self.version.tag)
-            else:
-                logging.error("An error occured while generating CK report for version " + self.version.tag)
+            logging.info('Metric Calculated')
+
+            return metric
