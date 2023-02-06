7060d02b17e8332ff175d356bae8ea6b9c9e1469
Aurelien ALET <aalet@sii.fr>
2023-02-06 11:17:47 +0100

    fix: add missing file

---
+++ utils/date.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def date_iso_8601_to_datetime(date_str: str):
+    return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+def datetime_to_date_hours_minuts(date_datetime: datetime):
+    return date_datetime.strftime("%Y-%m-%d %H:%M")
