19f83b26ab157d6a9b43c724ad044bb6e81a312c
Antoine Micard <33573640+Creadeyh@users.noreply.github.com>
2023-03-27 10:47:58 +0200

    Added rounded divide math method

--- utils/math.py
+++ utils/math.py
@@ -1,4 +1,5 @@
 from statistics import mean
+import logging
 
 class Math():
     nb_decimal_numbers = 2
@@ -12,3 +13,13 @@ class Math():
     def get_rounded_rate(cls, value, total):
         rate = (1. * value / total) * 100
         return round(rate, cls.nb_decimal_numbers)
+    
+    @classmethod
+    def get_rounded_divide(cls, dividend, divisor):
+        try:
+            quotient = round(dividend / divisor, cls.nb_decimal_numbers)
+        except ZeroDivisionError:
+            logging.exception("Math:Cannot divide by zero")
+            return None
+        else:
+            return quotient
